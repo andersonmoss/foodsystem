df5eae07d0eb52e7edf978acf6992ef1176dc8bd
Anderson Moss <andersonmoss@outlook.com.br>
2020-07-22 18:48:48 -0300

    List Plans

---
+++ app/Http/Controllers/Admin/PlanController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PlanController extends Controller
+{
+    private $repository;
+
+    public function __construct(Plan $plan){
+        $this->repository = $plan;
+    }
+
+    public function index(){
+        $plans = $this->repository->latest()->paginate();
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans,
+            
+        ]);
+    }
+}
