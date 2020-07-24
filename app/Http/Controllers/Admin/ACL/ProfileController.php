ce5454e284949ceef8b44324eb89d7af30aca3bf
Anderson Moss <andersonmoss@outlook.com.br>
2020-07-24 20:43:47 -0300

    Validate Profiles

--- app/Http/Controllers/Admin/ACL/ProfileController.php
+++ app/Http/Controllers/Admin/ACL/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\ACL;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateProfile;
 use App\Models\Profile;
 use Illuminate\Http\Request;
 
@@ -43,7 +44,7 @@ class ProfileController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateProfile $request)
     {
         $this->repository->create($request->all());
 
@@ -83,7 +84,7 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreUpdateProfile $request, $id)
     {
         $this->repository->find($id)->update($request->all());
 
